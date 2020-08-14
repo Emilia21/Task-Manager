034c59f46a8d1b16a6e7d78d3f36c3c1759fde5f
Emilia21 <epetkova96@gmail.com>
2020-08-14 12:26:22 +0300

    Updates

---
+++ precache-manifest.0a159d05b5f24b89a8c20bb643b0f04e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "167198d3b04e49323394ef164ed73b9b",
+    "url": "/Task-Manager/index.html"
+  },
+  {
+    "revision": "6b329d9d4ec126b4e609",
+    "url": "/Task-Manager/static/css/main.f7a83016.chunk.css"
+  },
+  {
+    "revision": "41de8d085a4d421051d8",
+    "url": "/Task-Manager/static/js/2.f2130bf5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Task-Manager/static/js/2.f2130bf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b329d9d4ec126b4e609",
+    "url": "/Task-Manager/static/js/main.8a7f6728.chunk.js"
+  },
+  {
+    "revision": "0d37581fae5ffad4769f",
+    "url": "/Task-Manager/static/js/runtime-main.470f931a.js"
+  }
+]);
